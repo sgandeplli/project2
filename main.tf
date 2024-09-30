a055f25ab453eadc5db6e584635870faec0cb601
aniljeenapati <jeenapati.anil@gmail.com>
2024-09-30 16:43:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,33 +1,36 @@
 provider "google" {
-  project     = "primal-gear-436812-t0"
-  region      = "us-central1"
+  project = "primal-gear-436812-t0"
+  region  = "us-central1"
 }
 
 resource "google_compute_instance" "centos_vm" {
   name         = "centos-vm"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
+
   boot_disk {
     initialize_params {
       image = "centos-cloud/centos-stream-9"  # Correct image family
     }
   }
+
   network_interface {
     network = "default"
     access_config {
     }
   }
+
   metadata = {
-    ssh-keys = "centos:${file("/root/.ssh/id_rsa.pub")}"
-    # Path where the SSH key will be stored inside the VM
-    ssh_key_path = "/home/centos/.ssh/authorized_keys"
+    ssh-keys = "centos:${file("/home/username/.ssh/id_rsa.pub")}"  # Update this path to your public key
   }
+  
   tags = ["http-server"]
 
   provisioner "local-exec" {
-    command = "echo ${self.network_interface.0.access_config.0.nat_ip} > ../ansible/inventory"
+    command = "echo ${self.network_interface.0.access_config.0.nat_ip} > ansible/inventory"  # Updated path
   }
 }
+
 output "vm_ip" {
   value = google_compute_instance.centos_vm.network_interface.0.access_config.0.nat_ip
 }
