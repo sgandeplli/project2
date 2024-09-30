3cf7acf782877832384c5474f3f6a86e81cc197d
aniljeenapati <jeenapati.anil@gmail.com>
2024-09-30 13:53:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "google_compute_instance" "centos_vm" {
 
   boot_disk {
     initialize_params {
-      image = "centos-cloud/centos-7"
+      image = "centos-cloud/centos-9"
     }
   }
 
